1d22e371feda3c9c585e2c2f998efe433096d513
sleepsyahs <xzhou01@bvsd.org>
2024-08-21 21:52:58 +0000

    Completed Assignment

--- Conversions.java
+++ Conversions.java
@@ -18,6 +18,8 @@ public class Conversions
         System.out.println("5. Ounces to Milliliters");
         System.out.println("6. Milliliters to Ounces");
         //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
+        System.out.println("7. Miles to Kilometers");
+        System.out.println("8. Kilometers to Miles");
         //You can add even more if you would like.
 
         int selection = keyboard.nextInt();
@@ -34,7 +36,66 @@ public class Conversions
         if (selection == 2)
         {
             //add the code to ask the user to enter Fahrenheit and then convert to Celsius
+            System.out.println("Enter Fahrenheit: ");
+            double fahrenheit = keyboard.nextDouble();
+            keyboard.nextLine();
+            double celsius = (fahrenheit-32)*(5.0/9);
+            System.out.println(fahrenheit + " degrees fahrenheit is " + celsius + " degrees celsius");
         }
         //add more if statements here.
+        if (selection == 3)
+        {
+            //Feet to Meters
+            System.out.println("Enter Feet: ");
+            double feet = keyboard.nextDouble();
+            keyboard.nextLine();
+            double meters = feet*0.3048;
+            System.out.println(feet + " feet is " + meters + " meters");
+        }
+        if (selection == 4)
+        {
+            //Meters to Feet
+            System.out.println("Enter Meters: ");
+            double meters = keyboard.nextDouble();
+            keyboard.nextLine();
+            double feet = meters*3.280839895;
+            System.out.println(meters + " meters is " + feet + " feet");
+        }
+        if (selection == 5)
+        {
+            //Ounces to Milliliters
+            System.out.println("Enter Ounces: ");
+            double ounces = keyboard.nextDouble();
+            keyboard.nextLine();
+            double milliliter = ounces*29.5735;
+            System.out.println(ounces + " ounces is " + milliliter + " millileters");
+        }
+        if (selection == 6)
+        {
+            //Milliliters to Ounces
+            System.out.println("Enter Milliliters: ");
+            double milliliters = keyboard.nextDouble();
+            keyboard.nextLine();
+            double ounces = milliliters/29.5735;
+            System.out.println(milliliters + " milliliters is " + ounces + " ounces");
+        }
+        if (selection == 7)
+        {
+            //Miles to Kilometers
+            System.out.println("Enter Miles: ");
+            double miles = keyboard.nextDouble();
+            keyboard.nextLine();
+            double kilometers = miles*1.60934;
+            System.out.println(miles + " miles is " + kilometers + " kilometers");
+        }
+        if (selection == 8)
+        {
+            //Kilometers to Miles
+            System.out.println("Enter Kilometers: ");
+            double kilometers = keyboard.nextDouble();
+            keyboard.nextLine();
+            double miles = kilometers/1.60934;
+            System.out.println(kilometers + " kilometerss is " + miles + " miles");
+        }
     }
 }
